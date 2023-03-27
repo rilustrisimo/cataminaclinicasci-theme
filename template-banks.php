32af1c897508050efc699c36e6dafdaa233fdfd7
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2023-03-27 13:48:00 +0800

    add banks

--- template-banks.php
+++ template-banks.php
@@ -20,20 +20,19 @@ if ( have_posts() ) : ?>
 	<?php while ( have_posts() ) { the_post(); ?>
 		<div class="page-single">
 			<main class="page-single__content" role="main">
-                <div class="custom-post__add-form" form-id="144">
-				    <?php $theme->createAcfForm(144, 'cashcheques', '<i class="fa-solid fa-plus"></i> Add Cash / Cheques'); ?>
+                <div class="custom-post__add-form" form-id="1197">
+				    <?php $theme->createAcfForm(1197, 'banks', '<i class="fa-solid fa-plus"></i> Add Bank'); ?>
                 </div>
                 <div class="custom-post__search"><div class="search-icon"><i class="fa-solid fa-magnifying-glass"></i></div><input type="text" class="search-ajax" placeholder="Search Name"></div>
-                <div class="custom-post__list" data-pt="cashcheques">
+                <div class="custom-post__list" data-pt="banks">
                     <?php 
                     $header = array(
                         'name_of_bank' => 'Name of Bank',
                         'account_number' => 'Account Number',
-                        'type_of_account' => 'Type',
-                        'amount' => 'Amount'
+                        'type_of_account' => 'Type'
                     );
 
-                    $theme->createCustomPostListHtml('cashcheques', 20, $header);
+                    $theme->createCustomPostListHtml('banks', 20, $header);
                     ?>
                 </div>
 			</main>
