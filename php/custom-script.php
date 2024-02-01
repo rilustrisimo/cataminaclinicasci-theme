304452c4c21d871c3d256d703f00e3b0bf3cf5c3
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-02-01 11:26:33 +0800

    catamina

--- php/custom-script.php
+++ php/custom-script.php
@@ -10,23 +10,22 @@ $meta_query = array(
 );
 
 $addquery = $theme->createQuery('supplies', $meta_query, -1, 'date', 'DESC');
-/*
-foreach($addquery as $p):
+$allsup = $addquery->posts;
+
+foreach($allsup as $p):
     $supid = $p->ID;
 
     $meta_query2 = array(
         'key'     => 'supply_name',
-        'value'   =>  $supid
+        'value'   =>  $supid 
     );
     
     $addquery2 = $theme->createQuery('actualsupplies', $meta_query2, -1, 'date', 'DESC');
 
     var_dump($addquery2->posts);
 endforeach;
-*/
 
-$addquery2 = $theme->createQuery('actualsupplies', array(), -1, 'date', 'DESC');
 
-    var_dump($addquery2->posts);
+
 
 ?>
