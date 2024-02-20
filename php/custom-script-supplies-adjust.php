423473713c880a49086ea4ff03e9194f5cd301e2
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-02-20 11:37:50 +0800

    catamina

--- php/custom-script-supplies-adjust.php
+++ php/custom-script-supplies-adjust.php
@@ -15,21 +15,37 @@ $args = array(
     'posts_per_page' => -1
 );
 
+$deptuser = array(
+    'NURSING' => 7,
+    'LABORATORY' => 6,
+    'PHARMACY' => 4,
+    'HOUSEKEEPING' => 8,
+    'MAINTENANCE' => 8,
+    'RADIOLOGY' => 5,
+    'BUSINESS OFFICE' => 9,
+    'INFORMATION / TRIAGE' => 10,
+    'PHYSICAL THERAPY' => 14,
+    'KONSULTA PROGRAM' => 11,
+    'CLINIC B' => 12,
+    'CLINIC C' => 12,
+    'CLINIC D' => 12
+);
 
-$args['author'] = 5;
+foreach($deptuser as $d => $i):
+    $args['author'] = $i;
 
+    $the_query = new WP_Query( $args );
+    $posts = $the_query->posts;
 
-$the_query = new WP_Query( $args );
-$posts = $the_query->posts;
-
-
-foreach($posts as $s):
-    $dept = get_field('department', $s->ID);
-    
-    if($dept != "RADIOLOGY"):
-        echo get_the_title($s->ID).' --> '.$dept.'<br>';
-    endif;
+    echo "------".$d."--------START--------------";
+    foreach($posts as $s):
+        $dept = get_field('department', $s->ID);
+        
+        if($dept != $d):
+            echo get_the_title($s->ID).' --> '.$dept.'<br>';
+        endif;
+    endforeach;
+    echo "------".$d."--------END--------------";
 endforeach;
-
 
 ?>
