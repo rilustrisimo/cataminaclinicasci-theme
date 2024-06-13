95a38f3c65567bd34dfc33489619b8535e189e0e
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2024-06-13 14:19:50 +0800

    add print

--- template-income-report.php
+++ template-income-report.php
@@ -79,7 +79,7 @@ if ( have_posts() ) : ?>
                         <div class="report__filter-btn"><a href="#" class="btn button"><i class="fa-solid fa-filter"></i> Apply Filter</a></div>
                     </form>
                 </div>
-                <div class="report__filter-btn" style="margin-bottom: 35px;"><input type="text" placeholder="Prepared By" name="preparedby" id="preparedby"><a href="#" class="btn button print-btn"><i class="fa-solid fa-print"></i> Print Report</a></div>
+                <div class="report__filter-btn" style="margin: 35px 0;"><input type="text" placeholder="Prepared By" name="preparedby" id="preparedby"><a href="#" class="btn button print-btn"><i class="fa-solid fa-print"></i> Print Report</a></div>
                 <div class="report__result" id="report__result">
                 <!-- result goes here -->
                 <?php echo $theme->getIncomeExpensesReport(date('01-m-Y'), date('d-m-Y', strtotime('last day of this month')), 'all', 'all');?>
