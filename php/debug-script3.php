33b273a9897cbcb330ba3208879a07f3cb914966
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2025-01-13 19:28:33 +0800

    fix

--- php/debug-script3.php
+++ php/debug-script3.php
@@ -4,7 +4,7 @@ require_once( __DIR__ . '/class-main.php' );
 
 $theme = new Theme();
 
-$res = $theme->getQtyOfSupplyAfterDate(2306, 'January 4, 2025', true);
+$res = $theme->getQtyOfSupplyAfterDate(2240, 'January 4, 2025', true);
 
 var_dump($res);
 ?>
