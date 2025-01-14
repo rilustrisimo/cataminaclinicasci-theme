b88e3d7ed74d42f73b584cd4f0753fe92d612962
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2025-01-14 14:24:41 +0800

    debug

--- php/class-main.php
+++ php/class-main.php
@@ -1432,6 +1432,8 @@ class Theme {
         $reconarray = array();
         $relsupplies = array();
         $datesupplies = array();
+
+        var_dump($batchData);
  
 
         foreach($batchData as $suppid => $supp):
