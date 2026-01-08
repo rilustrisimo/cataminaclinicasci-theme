9559ee8ba78fa3b10d8a2e748d4387a27c8922f0
Rouie Ilustrisimo <ilustrisimo.rouie@gmail.com>
2026-01-08 10:14:38 +0800

    hook debug log

--- includes/functions/enqueues.php
+++ includes/functions/enqueues.php
@@ -56,15 +56,16 @@ if ( ! function_exists( 'qed_init_theme_assets' ) ) {
 			wp_enqueue_script( 'counterup', PARENT_URL . '/assets/jslib/jquery.counterup.min.js', array( 'jquery', 'waypoints' ), '1.0', true );
 			wp_enqueue_script( 'vivus', PARENT_URL . '/assets/jslib/vivus.min.js', array(), '0.4.2', true );
 			
-			//wp_register_script( 'fitvid', PARENT_URL . '/assets/jslib/bxslider/vendor/jquery.fitvids.js', array( 'jquery' ), '1.0', true );
-			//wp_register_script( 'magnific-popup', PARENT_URL . '/assets/jslib/jquery.magnific-popup.min.js', array( 'jquery' ), '1.1.0', true );
+			wp_register_script( 'fitvid', PARENT_URL . '/assets/jslib/bxslider/vendor/jquery.fitvids.js', array( 'jquery' ), '1.0', true );
+			wp_register_script( 'magnific-popup', PARENT_URL . '/assets/jslib/jquery.magnific-popup.min.js', array( 'jquery' ), '1.1.0', true );
+			/*
 			wp_register_script( 'theme', PARENT_URL . '/assets/js/Theme.js', array( 'jquery' ), rand(), true );
 			wp_localize_script( 'theme', 'AjaxHelper', array(
 					'ajaxurl' => admin_url( 'admin-ajax.php' ),
 					'headerSectionNonce' => wp_create_nonce( 'swish-header-section-nonce' ),
 			) );
 			wp_enqueue_script( 'theme' );
-
+*/
 			if ( qed_get_option( 'show_header_search' ) ) {
 				wp_enqueue_style( 'magnific-popup' );
 				wp_enqueue_script( 'magnific-popup' );
